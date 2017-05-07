7e8336e7ec28a56468df294fb50dfe8bcb1e7ba5
kimnorth <benignhumour@gmail.com>
2017-05-07 18:18:32 +0100

    Created a GameContainer file and inserted into app.

---
+++ client/src/containers/GameContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class GameContainer extends React.Component {
+
+  render(){
+    return (
+
+      <div>
+      <h1>Guess Who</h1>
+      </div>
+
+    )
+  }
+
+}
+
+export default GameContainer;
