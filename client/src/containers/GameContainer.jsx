44515c61b210b0ebe04023d1645ebf2cd8db5058
kimnorth <benignhumour@gmail.com>
2017-05-07 18:29:09 +0100

    Created basic scoreboard for keeping track of wins - no logic yet.

--- client/src/containers/GameContainer.jsx
+++ client/src/containers/GameContainer.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 
 class GameContainer extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      wins: 0,
+      cardsInPlay: [],
+      cardsOutOfPlay: []
+    }
+  }
+
   render(){
     return (
 
       <div>
       <h1>Guess Who</h1>
+
+        <div id="scoreboard">
+          <p>Wins: {this.state.wins}</p>
+        </div>
+
+        <div id="main-board">
+          
+        </div>  
+
       </div>
 
     )
